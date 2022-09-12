617302b8a57787419ec0b3ba5ad202605fc5a085
John Baylor <john.baylor@gmail.com>
2022-09-11 20:41:35 -0700

    Convert CanvasSharedState to a GenServer, which made it easier to auto-save after a no-activity timeout

--- lib/spring83/canvas_shared_state.ex
+++ lib/spring83/canvas_shared_state.ex
@@ -1,47 +1,107 @@
 defmodule Spring83Web.CanvasSharedState do
-  use Agent
+  use GenServer
+  require Logger
+  alias Spring83.Canvas
 
   @width 16
   @height 16
   @last_cell @width * @height - 1
-  @default_canvas 0..@last_cell
-                  |> Enum.map(fn n -> {n, ""} end)
-  @default_state %{
-    canvas: @default_canvas,
-    user_count: 0
-  }
+  @default_canvas Enum.map(0..@last_cell, fn n -> {n, ""} end)
 
-  def start_link(options \\ %{}) do
-    {:ok, get_pid(options)}
+  # Client
+
+  def start_link(_options \\ %{}) do
+    pid =
+      case(GenServer.start_link(__MODULE__, %{}, name: {:global, __MODULE__})) do
+        {:ok, pid} -> pid
+        {:error, {:already_started, _pid}} -> raise("CanvasSharedState started twice?")
+      end
+
+    {:ok, pid}
+  end
+
+  def set_one_pixel(location, paint) do
+    GenServer.cast(get_pid(), {:set_one_pixel, location, paint})
+  end
+
+  def get_canvas do
+    GenServer.call(get_pid(), :get_canvas)
+  end
+
+  def set_user_count(delta) do
+    GenServer.cast(get_pid(), {:set_user_count, delta})
+  end
+
+  def get_user_count do
+    GenServer.call(get_pid(), :get_user_count)
+  end
+
+  # Server
+
+  @impl true
+  def init(_) do
+    canvas = Canvas.latest() || @default_canvas
+
+    {:ok,
+     %{
+       canvas: canvas,
+       user_count: 0,
+       autosave_pid: nil
+     }}
   end
 
   def get_pid(_options \\ %{}) do
-    case(Agent.start_link(fn -> @default_state end, name: __MODULE__)) do
-      {:ok, pid} -> pid
+    case(GenServer.start_link(__MODULE__, %{}, name: {:global, __MODULE__})) do
+      {:ok, pid} -> raise("CanvasSharedState should have been started already (pid #{inspect(pid)}})")
       {:error, {:already_started, pid}} -> pid
     end
   end
 
-  def set_one_pixel(location, paint) do
-    Agent.update(get_pid(), fn state = %{canvas: canvas} = state ->
-      %{state | canvas: List.update_at(canvas, location, fn _ -> {location, paint} end)}
-    end)
+  @impl true
+  def handle_call(:get_canvas, _from, %{canvas: canvas} = state) do
+    {:reply, canvas, state}
   end
 
-  def get_canvas do
-    %{canvas: canvas} = Agent.get(get_pid(), fn state -> state end)
-    canvas
+  @impl true
+  def handle_call(:get_user_count, _from, %{user_count: user_count} = state) do
+    {:reply, user_count, state}
   end
 
-  def set_user_count(delta) do
-    Agent.update(get_pid(), fn state = %{user_count: user_count} = state ->
-      user_count = Enum.max([user_count + delta, 0])
-      %{state | user_count: user_count}
-    end)
+  # TODO: rename to :update_user_count
+  @impl true
+  def handle_cast(
+        {:set_user_count, delta},
+        %{user_count: user_count} = state
+      ) do
+    {:noreply,
+     %{
+       state
+       | user_count: Enum.max([user_count + delta, 0])
+     }}
   end
 
-  def get_user_count do
-    %{user_count: user_count} = Agent.get(get_pid(), fn state -> state end)
-    user_count
+  @impl true
+  def handle_cast(
+        {:set_one_pixel, location, paint},
+        %{canvas: canvas, autosave_pid: autosave_pid} = state
+      ) do
+    # Reset our auto-save timer to be 2 minutes after this most-recent change
+    autosave_pid && Process.cancel_timer(autosave_pid)
+    autosave_pid = Process.send_after(self(), :save_canvas, :timer.minutes(2))
+
+    {:noreply,
+     %{
+       state
+       | canvas: List.update_at(canvas, location, fn _ -> {location, paint} end),
+         autosave_pid: autosave_pid
+     }}
+  end
+
+  @impl true
+  def handle_info(:save_canvas, %{canvas: canvas} =state) do
+    Canvas.save(canvas)
+    Logger.info("Canvas saved")
+
+    {:noreply, state}
   end
 end
