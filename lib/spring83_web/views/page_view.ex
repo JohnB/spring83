5aa719f36580e5eaf1fb9c7666b85d295fc01d83
John Baylor <john.baylor@gmail.com>
2022-10-23 23:30:26 -0700

    Revert "First pass on an unstyled modal overlay."
    This reverts commit c577c5421139993379cdf3ba4f0a81d25d566937.

--- lib/spring83_web/views/page_view.ex
+++ lib/spring83_web/views/page_view.ex
@@ -1,31 +1,10 @@
 defmodule Spring83Web.PageView do
   use Spring83Web, :view
-  use Phoenix.Component
 
   def color_picker_class(color, selected_color) do
     case color == selected_color do
       true -> "color-picker #{color} selected"
       _ -> "color-picker #{color}"
     end
-  end
-
-  def modal(assigns) do
-    ~H"""
-    <div class="modal">
-      <%= render_slot(@inner_block) %>
-    </div>
-    """
-  end
-
-  #  attr :project, :string
-#  attr :theme, :string
-  def learning(assigns) do
-    ~H"""
-    <div class="learning">
-      <span class="theme"><%= @theme %></span>
-      <span class="project"><%= @project %></span>
-      <%= render_slot(@inner_block) %>
-    </div>
-    """
   end
 end
