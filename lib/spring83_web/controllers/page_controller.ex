f43b227496f6d80b0841b81db07ee7aabc49d8c7
John Baylor <john.baylor@gmail.com>
2024-06-01 12:28:25 -0700

    Log the referer

--- lib/spring83_web/controllers/page_controller.ex
+++ lib/spring83_web/controllers/page_controller.ex
@@ -4,12 +4,15 @@ defmodule Spring83Web.PageController do
   require Logger
 
   def index(conn, _params) do
+    log_referer(conn, "PageController.index")
     dates_and_toppings = TodaysPizza.fetch_dates_and_topping()
     render(conn, "index.html", %{dates_and_toppings: dates_and_toppings})
   end
 
   def cal_greek(conn, _params) do
+    log_referer(conn, "PageController.cal_greek")
     Logger.info("conn.host: #{conn.host}}")
+
     past_present_future =
       Spring83.VenueCache.venue_list(:cal_greek)
       |> split_into_past_present_future()
@@ -22,6 +25,8 @@ defmodule Spring83Web.PageController do
   end
 
   def la_greek(conn, _params) do
+    log_referer(conn, "PageController.la_greek")
+
     past_present_future =
       Spring83.VenueCache.venue_list(:la_greek)
       |> split_into_past_present_future()
@@ -31,6 +36,11 @@ defmodule Spring83Web.PageController do
       "who_is_at_the.html",
       put_in(past_present_future, [:page_title], "LA Greek Theater")
     )
+  end
+
+  defp log_referer(conn, caller) do
+    referer = List.keyfind(conn.req_headers, "referer", 0, {"", "no referer"}) |> elem(1)
+    Logger.info("#{caller} referer=#{referer}}")
   end
 
   defp split_into_past_present_future(map) do
