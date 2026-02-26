deb0f40acb5ff14ad7961ff50d4cb52eb115b4cd
John Baylor <john.baylor@gmail.com>
2026-02-26 13:03:34 -0800

    Correctly update to Elixir 1.16

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Spring83.MixProject do
     [
       app: :spring83,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
