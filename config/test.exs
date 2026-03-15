3cf7e71a111301d4d7bb19e18b3183a077d98ee5
John Baylor <john.baylor@gmail.com>
2026-03-14 23:05:15 -0700

    Fix test harness compiler warning

--- config/test.exs
+++ config/test.exs
@@ -24,7 +24,7 @@ config :spring83, Spring83Web.Endpoint,
 # config :spring83, Spring83.Mailer, adapter: Swoosh.Adapters.Test
 
 # Print only warnings and errors during test
-config :logger, level: :warn
+config :logger, level: :warning
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
